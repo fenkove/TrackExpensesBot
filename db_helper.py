e5b47acec2acb20bbc060fa30d24d5ac4a5ad87d
Sergey Panasyuk <sergey.panasyuk@just-eat.com>
2020-05-19 02:54:52 +0300

    A little refactoring of db connection stuff

--- db_helper.py
+++ db_helper.py
@@ -1,5 +1,12 @@
 import pymongo
 import config
 
-def prepare_db_client():
-    return pymongo.MongoClient(config.MONGO_DB_URL)
+def prepare_main_collection():
+    client = pymongo.MongoClient(config.MONGO_DB_URL)
+    mydb = client[config.DB_NAME]
+    return mydb[config.DB_MAIN_TABLE]
+
+def prepare_categories_collection():
+    client = pymongo.MongoClient(config.MONGO_DB_URL)
+    mydb = client[config.DB_NAME]
+    return mydb[config.DB_CATEGORIES_TABLE]
